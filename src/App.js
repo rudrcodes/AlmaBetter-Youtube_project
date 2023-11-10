5330254e69756f0c447726be364477d6b8ff3055
Rudransh <rudrcodes@gmail.com>
2023-11-10 16:34:13 +0530

    Updated links

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="header px-2 py-3">
         <nav className="navbar navbar-expand-lg bg-dark">
           <div className="container-fluid">
-            <a href="/youtube-frontend" className="navbar-brand align-middle">
+            <a href="/" className="navbar-brand align-middle">
               <i className="fa-brands fa-youtube px-1"></i>
               Youtube
             </a>
@@ -19,19 +19,10 @@ function App() {
         </nav>
       </div>
       <Routes>
-        <Route path="/youtube-frontend" element={<Homepage />} />
-        <Route
-          path="/youtube-frontend/DisplayAllSubs"
-          element={<DisplayAllSubs />}
-        />
-        <Route
-          path="/youtube-frontend/DisplayNameAndSub"
-          element={<DisplayNameAndSub />}
-        />
-        <Route
-          path="/youtube-frontend/UpdateUser"
-          element={<UpdateUser />}
-        />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/DisplayAllSubs" element={<DisplayAllSubs />} />
+        <Route path="/DisplayNameAndSub" element={<DisplayNameAndSub />} />
+        <Route path="/UpdateUser" element={<UpdateUser />} />
       </Routes>
     </div>
   );
